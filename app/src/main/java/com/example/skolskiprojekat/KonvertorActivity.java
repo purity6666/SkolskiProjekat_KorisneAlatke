013759a316abcfe343371f96db11c20a3d274b7b
purity6666 <68863394+purity6666@users.noreply.github.com>
2021-05-09 22:57:55 +0200

    Glavni Commit

---
+++ app/src/main/java/com/example/skolskiprojekat/KonvertorActivity.java
@@ -0,0 +1,80 @@
+package com.example.skolskiprojekat;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.animation.IntArrayEvaluator;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class KonvertorActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button convertButton;
+    private Button tBtn;
+    private TextView bTV;
+    private TextView oTV;
+    private TextView hTV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_konvertor);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("Pretvaranje brojeva");
+
+        //dugmad
+        convertButton = findViewById(R.id.prevedi_btn);
+        tBtn = findViewById(R.id.tBtn);
+
+        //unos
+        editText = findViewById(R.id.editTextNumber);
+
+        //ispis
+        bTV = findViewById(R.id.binarno_tv);
+        oTV = findViewById(R.id.oktalno_tv);
+        hTV = findViewById(R.id.heksadecimalno_tv);
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onClick(View v) {
+                int broj = Integer.parseInt(editText.getText().toString());
+
+                bTV.setText("Binarno: " + Integer.toBinaryString(broj));
+                oTV.setText("Oktalno: " + Integer.toOctalString(broj));
+                hTV.setText("Heksadecimalno: " + Integer.toHexString(broj));
+
+                editText.getText().clear();
+            }
+        });
+
+        tBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://www.skolaprogramiranja.rs/prebacivanje-iz-dekadnog-u-binarni-oktalni-i-heksadecimalni-brojevni-sistem/")));
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        switch (id) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/com/example/skolskiprojekat/MainActivity.java
@@ -0,0 +1,55 @@
+package com.example.skolskiprojekat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button konvertor_btn;
+    private Button nesto_btn;
+    private Button eduis_btn;
+    private Button tutorialButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        getSupportActionBar().setTitle("Korisne Alatke");
+
+        //dugmad
+        eduis_btn = findViewById(R.id.eduis_btn);
+        eduis_btn.setOnClickListener(this);
+        nesto_btn = findViewById(R.id.rc_btn);
+        nesto_btn.setOnClickListener(this);
+        konvertor_btn = findViewById(R.id.pretvarac_btn);
+        konvertor_btn.setOnClickListener(this);
+        tutorialButton = findViewById(R.id.tutorijal_btn);
+        tutorialButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.eduis_btn:
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://eobrazovanje.skolers.org/")));
+                break;
+            case R.id.pretvarac_btn:
+                startActivity(new Intent(this, KonvertorActivity.class));
+                break;
+            case R.id.rc_btn:
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("https://edukacija.rs/matematika/osnovna-skola")));
+                break;
+            case R.id.tutorijal_btn:
+                startActivity(new Intent(Intent.ACTION_VIEW).setData(Uri.parse("http://majmune.com/?i=1")));
+                break;
+            default:
+                break;
+        }
+    }
+}
